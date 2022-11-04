51c0b356a9ff1cc5eb03251789932aa73d781929
Dhananjay12 <dhananjayavare786@gmail.com>
2022-11-04 20:54:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,297 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import logo from "./Image/logo.png";
+import banner from "./Image/banner.jpg";
+import cl from "./Image/cl-1.png";
+import longcloud from "./Image/LOWER-CLOUD.png";
+import { MdArrowDropDown } from "react-icons/md";
+import { RiArrowDropRightFill } from "react-icons/ri";
 
 function App() {
+  const [showMediaIcons, setShowMediaIcons] = useState(false);
+  const [color, setColor] = useState(false);
+
+  const changeColor = () => {
+    if (window.scrollY >= 50) {
+      setColor(true);
+    } else {
+      setColor(false);
+    }
+  };
+  window.addEventListener("scroll", changeColor);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <>
+      <nav className={showMediaIcons ? "navbar active" : "navbars"}>
+        <a href="/" className="">
+          <img src={logo} alt="error" className="logo" />
         </a>
-      </header>
-    </div>
+        <ul className="menu">
+          <li>
+            <div className="dropdown">
+              <a href="/" className="dropbtn">
+                Business-Guide <MdArrowDropDown />
+              </a>
+              <div className="dropdown-content">
+                <li>
+                  <a href="/">Where</a>{" "}
+                </li>
+                <li>
+                  <a href="/">How</a>{" "}
+                </li>
+                <li>
+                  <a href="/">May</a>{" "}
+                </li>
+              </div>
+            </div>
+          </li>
+
+          <li>
+            <div className="dropdown">
+              <a href="/" className="dropbtn">
+                Licensing <MdArrowDropDown />
+              </a>
+              <div className="dropdown-content">
+                <li className="dropdown2">
+                  <a href="/" className="d-flex">
+                    Mainland License{" "}
+                    <RiArrowDropRightFill fontSize="20px" className="mt-1 " />{" "}
+                  </a>
+                  <ul className="dropdown-content2">
+                    <li>
+                      <a href="/">Professional</a>{" "}
+                    </li>
+                    <li>
+                      <a href="/">Commercial License</a>{" "}
+                    </li>
+                    <li>
+                      <a href="/">Industrial License</a>{" "}
+                    </li>
+                    <li>
+                      <a href="/">Tourism License</a>{" "}
+                    </li>
+                    <li>
+                      <a href="/">E Trader Lincense</a>{" "}
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <a href="/">Free zone License</a>{" "}
+                </li>
+                <li>
+                  <a href="/">Offshore License</a>{" "}
+                </li>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div className="dropdown">
+              <a href="/" className="dropbtn">
+                Visa <MdArrowDropDown />
+              </a>
+              <div className="dropdown-content">
+                <li>
+                  <a href="/">Dubai-Business visa</a>{" "}
+                </li>
+                <li>
+                  <a href="/">Dubai-Investi visa</a>{" "}
+                </li>
+              </div>
+            </div>
+          </li>
+          <li>
+            <a href="/">About US </a>{" "}
+          </li>
+          <li>
+            <a href="/">Blog </a>{" "}
+          </li>
+        </ul>
+
+        <button
+          className="display  mx-3"
+          onClick={() => setShowMediaIcons(false)}
+        />
+      </nav>
+      <div className="hamburger-menu mt-1">
+        <a href="/" onClick={() => setShowMediaIcons(!showMediaIcons)}>
+          <button className="icons" />
+        </a>
+      </div>
+
+      <div>
+        <div className="sky1 img-fluid">
+          <img height="50px" className="cloud-1" src={cl}></img>
+          <img height="50px" className="cloud-5" src={cl}></img>
+          <img height="50px" className="cloud-4" src={cl}></img>
+          <img height="50px" className="cloud-3" src={cl}></img>
+          <img height="50px" className="cloud-2" src={cl}></img>
+        </div>
+        <div style={{ marginTop: "-11rem" }}>
+          <img className="cloud-10" src={longcloud}></img>
+          <img className="cloud-6" src={longcloud}></img>
+          <img className="cloud-8" src={longcloud}></img>
+          <img className="cloud-7" src={longcloud}></img>
+          <img className="cloud-9" src={longcloud}></img>
+        </div>
+      </div>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+
+      <div className="mt-5">
+        <p>
+          orem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum. Why do we use it? It is a long
+          established fact that a reader will be distracted by the readable
+          content of a page when looking at its layout. The point of using Lorem
+          Ipsum is that it has a more-or-less normal distribution of letters, as
+          opposed to using 'Content here, content here', making it look like
+          readable English. Many desktop publishing packages and web page
+          editors now use Lorem Ipsum as their default model text, and a search
+          for 'lorem ipsum' will uncover many web sites still in their infancy.
+          Various versions have evolved over the years, sometimes by accident,
+          sometimes on purpose (injected humour and the like). orem Ipsum is
+          simply dummy text of the printing and typesetting industry. Lorem
+          Ipsum has been the industry's standard dummy text ever since the
+          1500s, when an unknown printer took a galley of type and scrambled it
+          to make a type specimen book. It has survived not only five centuries,
+          but also the leap into electronic typesetting, remaining essentially
+          unchanged. It was popularised in the 1960s with the release of
+          Letraset sheets containing Lorem Ipsum passages, and more recently
+          with desktop publishing software like Aldus PageMaker including
+          versions of Lorem Ipsum. Why do we use it? It is a long established
+          fact that a reader will be distracted by the readable content of a
+          page when looking at its layout. The point of using Lorem Ipsum is
+          that it has a more-or-less normal distribution of letters, as opposed
+          to using 'Content here, content here', making it look like readable
+          English. Many desktop publishing packages and web page editors now use
+          Lorem Ipsum as their default model text, and a search for 'lorem
+          ipsum' will uncover many web sites still in their infancy. Various
+          versions have evolved over the years, sometimes by accident, sometimes
+          on purpose (injected humour and the like). orem Ipsum is simply dummy
+          text of the printing and typesetting industry. Lorem Ipsum has been
+          the industry's standard dummy text ever since the 1500s, when an
+          unknown printer took a galley of type and scrambled it to make a type
+          specimen book. It has survived not only five centuries, but also the
+          leap into electronic typesetting, remaining essentially unchanged. It
+          was popularised in the 1960s with the release of Letraset sheets
+          containing Lorem Ipsum passages, and more recently with desktop
+          publishing software like Aldus PageMaker including versions of Lorem
+          Ipsum. Why do we use it? It is a long established fact that a reader
+          orem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum. Why do we use it? It is a long
+          established fact that a reader will be distracted by the readable
+          content of a page when looking at its layout. The point of using Lorem
+          Ipsum is that it has a more-or-less normal distribution of letters, as
+          opposed to using 'Content here, content here', making it look like
+          readable English. Many desktop publishing packages and web page
+          editors now use Lorem Ipsum as their default model text, and a search
+          for 'lorem ipsum' will uncover many web sites still in their infancy.
+          Various versions have evolved over the years, sometimes by accident,
+          sometimes on purpose (injected humour and the like). will be orem
+          Ipsum is simply dummy text of the printing and typesetting industry.
+          Lorem Ipsum has been the industry's standard dummy text ever since the
+          1500s, when an unknown printer took a galley of type and scrambled it
+          to make a type specimen book. It has survived not only five centuries,
+          orem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum. Why do we use it? It is a long
+          established fact that a reader will be distracted by the readable
+          content of a page when looking at its layout. The point of using Lorem
+          Ipsum is that it has a more-or-less normal distribution of letters, as
+          opposed to using 'Content here, content here', making it look like
+          readable English. Many desktop publishing packages and web page
+          editors now use Lorem Ipsum as their default model text, and a search
+          for 'lorem ipsum' will uncover many web sites still in their infancy.
+          Various versions have evolved over the years, sometimes by accident,
+          sometimes on purpose (injected humour and the like). but also the leap
+          into electronic typesetting, remaining essentially unchanged. It was
+          popularised in the 1960s with the release of Letraset sheets
+          containing Lorem Ipsum passages, and more recently with desktop
+          publishing software like Aldus PageMaker including versions of Lorem
+          Ipsum. Why do we use it? It is a long established fact that a reader
+          will be distracted by the readable content of a page when looking at
+          its layout. The point of using Lorem Ipsum is that it has a
+          more-or-less normal distribution of letters, as opposed to using
+          'Content here, content here', making it look like readable English.
+          Many desktop publishing packages and web page editors now use Lorem
+          Ipsum as their default model text, and a search for 'lorem ipsum' will
+          uncover many web sites still in their infancy. Various versions have
+          evolved over the years, sometimes by accident, sometimes on purpose
+          (injected humour and the like). orem Ipsum is simply dummy text of the
+          printing and typesetting industry. Lorem Ipsum has been the industry's
+          standard dummy text ever since the 1500s, when an unknown printer took
+          a galley of type and scrambled it to make a type specimen book. It has
+          survived not only five centuries, but also the leap into electronic
+          typesetting, remaining essentially unchanged. It was popularised in
+          the 1960s with the release of Letraset sheets containing Lorem Ipsum
+          passages, and more recently with desktop publishing software like
+          Aldus PageMaker including versions of Lorem Ipsum. Why do we use it?
+          It is a long established fact that a reader will be distracted by the
+          readable content of a page when looking at its layout. The point of
+          using Lorem Ipsum is that it has a more-or-less normal distribution of
+          letters, as opposed to using 'Content here, content here', making it
+          look like readable English. Many desktop publishing packages and web
+          page editors now use Lorem Ipsum as their default model text, and a
+          search for 'lorem ipsum' will uncover many web sites still in their
+          infancy. Various versions have evolved over the years, sometimes by
+          accident, sometimes on purpose (injected humour and the like).
+          distracted by the readable content of a page when looking at its
+          layout. The point of using Lorem Ipsum is that it has a more-or-less
+          normal distribution of letters, as opposed to using 'Content here,
+          content here', making it look like readable English. Many desktop
+          publishing packages and web page editors now use Lorem Ipsum as their
+          default model text, and a search for 'lorem ipsum' will uncover many
+          web sites still in their infancy. Various versions have evolved over
+          the years, sometimes by accident, sometimes on purpose (injected
+          humour and the like). orem Ipsum is simply dummy text of the printing
+          and typesetting industry. Lorem Ipsum has been the industry's standard
+          dummy text ever since the 1500s, when an unknown printer took a galley
+          of type and scrambled it to make a type specimen book. It has survived
+          not only five centuries, but also the leap into electronic
+          typesetting, remaining essentially unchanged. It was popularised in
+          the 1960s with the release of Letraset sheets containing Lorem Ipsum
+          passages, and more recently with desktop publishing software like
+          Aldus PageMaker including versions of Lorem Ipsum. Why do we use it?
+          It is a long established fact that a reader will be distracted by the
+          readable content of a page when looking at its layout. The point of
+          using Lorem Ipsum is that it has a more-or-less normal distribution of
+          letters, as opposed to using 'Content here, content here', making it
+          look like readable English. Many desktop publishing packages and web
+          page editors now use Lorem Ipsum as their default model text, and a
+          search for 'lorem ipsum' will uncover many web sites still in their
+          infancy. Various versions have evolved over the years, sometimes by
+          accident, sometimes on purpose (injected humour and the like).
+        </p>
+      </div>
+    </>
   );
 }
 
